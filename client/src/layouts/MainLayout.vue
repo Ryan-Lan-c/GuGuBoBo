d2530e5e17cd52ac2e79c0a4a4181a06fa588ae7
Ryan-Lan-c <passersbys@gmail.com>
2023-02-25 20:56:33 +0800

    init project

---
+++ client/src/layouts/MainLayout.vue
@@ -0,0 +1,111 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        >
+          <q-menu dark>
+            <q-list style="min-width: 400px">
+              <q-item clickable v-close-popup>
+                <q-item-section>New tab</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>New incognito tab</q-item-section>
+              </q-item>
+              <q-separator />
+              <q-item clickable v-close-popup>
+                <q-item-section>Recent tabs</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>History</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>Downloads</q-item-section>
+              </q-item>
+              <q-separator />
+              <q-item clickable v-close-popup>
+                <q-item-section>Settings</q-item-section>
+              </q-item>
+              <q-separator />
+              <q-item clickable v-close-popup>
+                <q-item-section>Help &amp; Feedback</q-item-section>
+              </q-item>
+            </q-list></q-menu
+          ></q-btn
+        >
+
+        <q-toolbar-title> Cinema Rating </q-toolbar-title>
+
+        <div>something here maybe member login</div>
+      </q-toolbar>
+    </q-header>
+
+    <!-- <q-drawer v-model="leftDrawerOpen" bordered>
+      <q-list>
+        <q-item-label header> you can say something here </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer> -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Movie",
+    caption: "some movies",
+    icon: "movie_filter",
+    link: "https://www.google.com",
+  },
+  {
+    title: "TV series / Drama",
+    caption: "TV series / Drama",
+    icon: "theaters",
+    link: "https://www.google.com",
+  },
+  {
+    title: "Short episode",
+    caption: "Short episode",
+    icon: "live_tv",
+    link: "https://www.google.com",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    // EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      // essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
